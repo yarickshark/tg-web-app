75d9a0e3bfd6296f1f2745f97eb09e0eddf52941
yarickshark <asteroid152008@gmail.com>
2023-08-11 22:22:14 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import {useEffect} from "react";
+const tg = window.Telegram.WebApp;
+
+function App() {
+
+  useEffect( () => {
+    tg.ready()
+  }, [])
+
+  const onClose = () => {
+    tg.close()
+  }
+
+  return (
+    <div className="App">
+    work
+      <button onClick={onClose}>Закрити</button>
+    </div>
+  );
+}
+
+export default App;
