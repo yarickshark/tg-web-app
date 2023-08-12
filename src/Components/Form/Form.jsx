d3ccd2aaaaade81b38b7cb65207a5cf1e081888c
yarickshark <asteroid152008@gmail.com>
2023-08-12 10:14:53 +0300

    init

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -2,9 +2,43 @@ import React from 'react';
 import './Form.css';
 
 const Form = () => {
+    const [country, setCountry] = useState('');
+    const [street, setStreet] = useState('');
+    const [subject, setSubject] = useState('physical');
+
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value)
+    }
+
+    const onChangeStreet = (e) => {
+        setStreet(e.target.value)
+    }
+
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value)
+    }
+
     return (
-        <div>
-            FORM
+        <div className={"form"}>
+            <h3>Введіть ваші данні</h3>
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Країна'}
+                value={country}
+                onChange={onChangeCountry}
+            />
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Вулиця'}
+                value={street}
+                onChange={onChangeStreet}
+            />
+            <select value={subject} onChange={onChangeSubject} className={'select'}>
+                <option value={'physical'}>Фіз. лице</option>
+                <option value={'legal'}>Юр. лице</option>
+            </select>
         </div>
     );
 };
