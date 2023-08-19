6fd90aedb95a727638327fb37829d93c6532f6a2
yarickshark <asteroid152008@gmail.com>
2023-08-20 02:14:47 +0300

    init

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -6,17 +6,17 @@ export function useTelegram() {
         tg.close()
     }
 
-    const onToggleButton = () => {
-        if(tg.MainButton.isVisible) {
-            tg.MainButton.hide();
-        } else {
-            tg.MainButton.show();
-        }
-    }
+//    const onToggleButton = () => {
+//        if(tg.MainButton.isVisible) {
+//            tg.MainButton.hide();
+//        } else {
+//            tg.MainButton.show();
+//        }
+//    }
 
     return {
         onClose,
-        onToggleButton,
+       // onToggleButton,
         tg,
         user: tg.initDataUnsafe?.user,
     }
